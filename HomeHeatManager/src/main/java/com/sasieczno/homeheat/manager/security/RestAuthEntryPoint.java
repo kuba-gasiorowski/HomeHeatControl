5adbe7bcee8cfff59c3de4c096e6e563e4659061
Kuba Gasiorowski <kuba.gasiorowski@eu.rsystems.com>
2025-02-21 14:44:28 +0100

    Fixed the commence method to write value at the end as it effectively sends the response back. Ammending Content-Type. Autowired annotation replaced with constructor.

--- HomeHeatManager/src/main/java/com/sasieczno/homeheat/manager/security/RestAuthEntryPoint.java
+++ HomeHeatManager/src/main/java/com/sasieczno/homeheat/manager/security/RestAuthEntryPoint.java
@@ -1,9 +1,11 @@
 package com.sasieczno.homeheat.manager.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -14,19 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class RestAuthEntryPoint implements AuthenticationEntryPoint {
 
-    @Autowired
-    ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
         log.debug("Authentication error", e);
         AuthError authError = new AuthError(e.getMessage());
-        objectMapper.writeValue(httpServletResponse.getOutputStream(), objectMapper.writeValueAsString(authError));
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
         httpServletResponse.setHeader(HttpHeaders.WWW_AUTHENTICATE, "Bearer Realm: \"HeatManager\"");
         httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        objectMapper.writeValue(httpServletResponse.getOutputStream(), authError);
         httpServletResponse.flushBuffer();
     }
 }
